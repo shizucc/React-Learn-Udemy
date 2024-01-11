b80e50e2613ba45ef4cc5e7df452f8ecb56d35b3
shizucc <syifanaalfi@gmail.com>
2024-01-11 18:28:18 +0700

    new project

---
+++ 11-project-management-app/src/App.jsx
@@ -0,0 +1,12 @@
+import SideAppbar from "./components/SideAppBar";
+
+function App() {
+  return (
+    <div className="flex flex-row gap-3 h-screen">
+      <SideAppbar />
+      {/* <h1 className="my-8 text-center text-5xl font-bold ">Hello World</h1> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ 11-project-management-app/src/components/SideAppbar.jsx
@@ -0,0 +1,17 @@
+export default function SideAppbar() {
+  return (
+    <aside className="bg-gray-900 w-70 mt-10 rounded-tr-xl p-8">
+      <h1 className="text-slate-50 text-2xl font-semibold">YOUR PROJECTS</h1>
+
+      <button className="bg-gray-700 hover:bg-gray-300 rounded-md px-3 py-1 mt-6">
+        <h2 className="text-gray-400">+ Add Project</h2>
+      </button>
+
+      <section className="mt-8">
+        <button className="text-slate-400 hover:bg-gray-500  w-full text-start py-1 px-2 rounded-sm active:bg-gray-500">
+          Project 1
+        </button>
+      </section>
+    </aside>
+  );
+}
